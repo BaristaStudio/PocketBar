d281bc875e1af6e87d9fd39b6dd8d5e80258a77c
francinelucca <francinelucca@hotmail.com>
2020-03-07 20:12:01 -0400

    Project SetUp (MVVM, Prism, Font Awesome, Basic Assets, General Architecture

---
+++ PocketBar/PocketBar/Utils/Icon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PocketBar.Utils
+{
+	public static class Icon
+	{
+		public static readonly string Check = "\uf00c";
+		public static readonly string PX = "\uf26e";
+		public static readonly string AddressBook = "\uf2b9";
+	}
+}
