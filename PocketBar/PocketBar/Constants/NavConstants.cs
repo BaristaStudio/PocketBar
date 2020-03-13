0fda8b399b94076bbf178345cedc962ed5a03a36
FDQM <franklindavid.quezadamartinez@gmail.com>
2020-03-13 13:53:44 -0400

    Views constants

--- PocketBar/PocketBar/Constants/NavConstants.cs
+++ PocketBar/PocketBar/Constants/NavConstants.cs
@@ -6,6 +6,15 @@ namespace PocketBar.Constants
 {
 	public static class NavConstants
 	{
+		public const string CategoriesPage = "CategoriesPage";
+		public const string CocktailDetailsPage = "CocktailDetailsPage";
+		public const string CocktailsListPage = "CocktailsListPage";
+		public const string GlassesListPage = "GlassesListPage";
+		public const string IngredientDetailsPage = "IngredientDetailsPage";
+		public const string IngredientListPage = "IngredientListPage";
+		public const string MyFavoritesPage = "MyFavoritesPage";
+		public const string SurpriseMePage = "SurpriseMePage";
+
 		public const string PlaygroundPage = "PlaygroundPage";
 	}
 }
