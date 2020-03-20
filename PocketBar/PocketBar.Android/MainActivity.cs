a646ea7fc42fe5ed9f935a50d53094c1d4926473
onunez <onunez@camsoft.com.do>
2020-03-20 15:29:21 -0400

    Splash Screen Android

--- PocketBar/PocketBar.Android/MainActivity.cs
+++ PocketBar/PocketBar.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Prism.Ioc;
 
 namespace PocketBar.Droid
 {
-    [Activity(Label = "PocketBar", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "PocketBar", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ PocketBar/PocketBar.Android/SplashActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+
+namespace PocketBar.Droid
+{
+    [Activity(Label = "PocketBar", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : FormsAppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+
+        // Simulates background work that happens behind the splash screen
+        void SimulateStartup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
