c7ded8ec1d7497e91d8070d0650ae682c216c417
FDQM <franklindavid.quezadamartinez@gmail.com>
2020-03-25 18:30:44 -0400

    merge fixes

--- PocketBar/PocketBar/ViewModels/CocktailsListPageViewModel.cs
+++ PocketBar/PocketBar/ViewModels/CocktailsListPageViewModel.cs
@@ -1,6 +1,7 @@
 using PocketBar.Constants;
 using PocketBar.Managers;
 using PocketBar.Models;
+using Prism.Commands;
 using Prism.Navigation;
 using Prism.Services;
 using System;
@@ -14,14 +15,15 @@ namespace PocketBar.ViewModels
     class CocktailsListPageViewModel: BaseViewModel
 	{
 		public ObservableCollection <Cocktail> Cocktails { get; set; }
-    private CategoriesManager categoriesManager;
+		private CategoriesManager categoriesManager;
 		private GlassesManager glassesManager;
 		private IngredientsManager ingredientsManager;
 
 
 		public CocktailsListPageViewModel(PageDialogService pageDialogService, INavigationService navigationService,CategoriesManager categoriesManager, GlassesManager glassesManager, IngredientsManager ingredientsManager) : base(pageDialogService, navigationService)
         {
-            this.categoriesManager = categoriesManager;
+
+			this.categoriesManager = categoriesManager;
             this.glassesManager = glassesManager;
             this.ingredientsManager = ingredientsManager;
 
@@ -88,5 +90,63 @@ namespace PocketBar.ViewModels
 
 		}
 
+		public async void Initialize(INavigationParameters parameters)
+		{
+			try
+			{
+				SearchType? type;
+				string searchTerm = "";
+				string Title = "";
+				if (parameters == null || parameters.Count == 0) return;
+				if (parameters.ContainsKey("type"))
+				{
+					type = (SearchType)parameters["type"];
+				}
+				else
+				{
+					await ShowMessage(ErrorMessages.ErrorOccured, ErrorMessages.MissingInformation, ErrorMessages.Ok);
+					return;
+				}
+
+				if (parameters.ContainsKey("searchTerm"))
+				{
+					searchTerm = parameters["searchTerm"] as string;
+				}
+				else
+				{
+					await ShowMessage(ErrorMessages.ErrorOccured, ErrorMessages.MissingInformation, ErrorMessages.Ok);
+					return;
+				}
+
+				if (parameters.ContainsKey("title"))
+				{
+					Title = parameters.GetValue<string>("title");
+				}
+				else
+				{
+					await ShowMessage(ErrorMessages.ErrorOccured, ErrorMessages.MissingInformation, ErrorMessages.Ok);
+					return;
+				}
+
+				if(type != null && !string.IsNullOrEmpty(searchTerm))
+				{
+					switch (type)
+					{
+						case SearchType.Category:
+							GetCocktailsByCategory(searchTerm);
+							break;
+						case SearchType.Glass:
+							GetCocktailsByGlass(searchTerm);
+							break;
+						case SearchType.Ingredient:
+							GetCocktailsByIngredient(searchTerm);
+							break;
+					}
+				}
+			}catch(Exception e)
+			{
+				await this.ShowMessage(ErrorMessages.ErrorOccured, e.Message, ErrorMessages.Ok);
+			}
+		}
 	}
 }
