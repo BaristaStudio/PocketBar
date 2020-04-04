cbf6ff3a37e2a981ad1f18063eb65a6b0b92111c
Jonathan Taveras <jonathanrtv@outlook.com>
2020-04-03 22:57:49 -0400

    ios optimizations

--- PocketBar/PocketBar/Controls/Common/ImageOption.xaml.cs
+++ PocketBar/PocketBar/Controls/Common/ImageOption.xaml.cs
@@ -14,11 +14,11 @@ namespace PocketBar.Controls.Common
 	public partial class ImageOption : ContentView
 	{
 		public static readonly BindableProperty OptionNameProperty = BindableProperty.Create(nameof(OptionName), typeof(string), typeof(ImageOption), default(string));
-		public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(nameof(ImageSrc), typeof(ImageSource), typeof(ImageOption), default(ImageSource));
+		public static readonly BindableProperty ImageSrcProperty = BindableProperty.Create(nameof(ImageSrc), typeof(ImageSource), typeof(ImageOption), default(ImageSource));
 		public static readonly BindableProperty OptionSelectedCommandProperty = BindableProperty.Create(nameof(OptionSelectedCommand), typeof(DelegateCommand<ImageOption>), typeof(ImageOption), default(DelegateCommand));
-
+		
 		public string OptionName { get => (string)GetValue(OptionNameProperty); set => SetValue(OptionNameProperty, value); }
-		public ImageSource ImageSrc { get => (ImageSource)GetValue(ImageSourceProperty); set => SetValue(ImageSourceProperty, value); }
+		public ImageSource ImageSrc { get => (ImageSource)GetValue(ImageSrcProperty); set => SetValue(ImageSrcProperty, value); }
 		public DelegateCommand<ImageOption> OptionSelectedCommand { get => (DelegateCommand<ImageOption>)GetValue(OptionSelectedCommandProperty); set => SetValue(OptionSelectedCommandProperty, value); }
 		public ImageOption()
 		{
